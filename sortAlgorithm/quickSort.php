3b6ed06d5048873c91d01459c937153634f24c66
JakkieChenFei <744547287@qq.com>
2018-03-25 21:46:08 +0800

    quicksort

--- sortAlgorithm/quickSort.php
+++ sortAlgorithm/quickSort.php
@@ -1,11 +1,30 @@
 <?php
-function quickSort()
+$disorderQueue = [3,2,38,5,15,36,26,23,4,25,65,21,42,34];
+function quickSort($left, $right)
 {
-    $disorderQueue = [3, 33, 8421, 492, 2, 401, 23];
-    $size = sizeof($disorderQueue);
-    for ($i = 0; $i < $size; $i++) { 
-        
+    $i = $left;
+    $j = $right;
+    if ($left >= $right) {
+        return;
     }
-    print_r($disorderQueue);
+    $temp = $GLOBALS['disorderQueue'][$left];
+    while ($i < $j) {
+        if ($temp > $GLOBALS['disorderQueue'][$j]) {
+            $GLOBALS['disorderQueue'][$i] = $GLOBALS['disorderQueue'][$j];
+            $GLOBALS['disorderQueue'][$j] = $temp;
+            while ($i < $j) {
+                if ($temp < $GLOBALS['disorderQueue'][$i]) {
+                    $GLOBALS['disorderQueue'][$j] = $GLOBALS['disorderQueue'][$i];
+                    $GLOBALS['disorderQueue'][$i] = $temp;
+                    break;
+                }
+                $i++;
+            }
+        }
+        $j--;
+    }
+    quickSort($left, $i);
+    quickSort($i + 1, $right);
 }
-quickSort();
+quickSort(0, sizeof($GLOBALS['disorderQueue']) - 1);
+print_r($GLOBALS['disorderQueue']);
